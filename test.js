4ed9330bcb0cf1abd9fc3db42d22734f382fbad6
Mafarifironfon <mafarifironfon@hotmail.fr>
2020-01-10 10:52:52 +0100

    Create checkbox

--- test.js
+++ test.js
@@ -1,35 +1,52 @@
-$(function() {
+$(function () {
 
-    var $list, $newItemForm;
-    $list = $('ul');
-    $newItemForm = $('#newItemForm');
+  var $list, $newItemForm;
+  $list = $('ul');
+  $newItemForm = $('#newItemForm');
 
-  
-    $newItemForm.on('submit', function(e) {
-      e.preventDefault();
-      var text = $('input:text').val();
-      $list.append(`
+  $checkbox2 = $(".check2");
+
+  $newItemForm.on('submit', function (e) {
+    e.preventDefault();
+    var text = $('input:text').val();
+    $list.append(`
         <li class="item">
           <div class="item-inner">${ text }</div>
             <input type="submit" class="delete" value="delete" />
             <div class="chexbox-inner">
-              <input type="checkbox" class="check1">
-              <label for="subscribeNews">En cours</label>
-              <input type="checkbox" class="check2">
-              <label for="subscribeNews">Terminé</label>
+              <input type="checkbox" name="super" class="check1" id="check1">
+              <label for="check1">En cours</label>
+              <input type="checkbox" class="check2" id="check2">
+              <label for="check2">Terminé</label>
             </div>
         </li>`);
-      $('input:text').val('');
-    });
-  
-    $list.on('click', '.delete', function() {
-      var $this = $(this);
-      $this.parent().remove();
-    });
-
-    $list.on('click', '.check1', function() {
-      var $change = $(this).parents(".item");
-      $change.addClass("todo");
-    });
-  
+    $('input:text').val('');
   });
+
+  // if ($('input[name=super]').attr(':checked')) {
+    $list.on('click', '.check1', function () {
+      color("todo", this);
+      
+    });
+    // }
+    // else {
+    $list.on('click', '.check2', function () {
+      color("done", this);
+    });
+  // }
+function color(className, element){
+  const isChecked = element.checked;
+      var $change = $(element).parents(".item");
+      if(isChecked){
+        $change.addClass(className);
+      }else{
+        $change.removeClass(className);
+      }
+}
+
+  $list.on('click', '.delete', function () {
+    var $this = $(this);
+    $this.parent().remove();
+  });
+
+});
